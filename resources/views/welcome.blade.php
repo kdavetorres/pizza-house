8ad5f6bbd4ae4a24c677b2a335704b92d1a9cd63
Azriel <aoirres@gmail.com>
2021-07-27 10:46:39 +0800

    working on webpack sass configuration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="content">
     <div class="title text-center m-b-md">
-        <img src="/img/pizza-house.jpg" alt="pizza house logo" id="pizza-logo">
+        <img src="/img/pizza-house.jpg" alt="pizza house logo" id="pizza-logo" width="320">
         <h1>Pizza House</h1>
     </div>
     <p class="mssg">{{ session('mssg') }}</p>
