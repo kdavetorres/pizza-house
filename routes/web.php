fedc7dc670f16f2db8c941780163d25979d6b3e4
Azriel <aoirres@gmail.com>
2021-07-27 12:15:35 +0800

    laravel authentication setup

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::get('/pizzas/create', [PizzaController::class, 'create']);
 Route::get('/pizzas/{id}', [PizzaController::class, 'show']);
 Route::post('/pizzas', [PizzaController::class, 'store']);
 Route::delete('/pizzas/{id}', [PizzaController::class, 'destroy']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
